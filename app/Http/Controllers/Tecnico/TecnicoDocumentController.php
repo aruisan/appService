8693f0e8d82c184b0840bfc502ea1c4dd2bc4e06
Jhoysner <jhoysner@hotmail.com>
2019-01-14 01:19:05 -0400

    dsadsa

--- app/Http/Controllers/Tecnico/TecnicoDocumentController.php
+++ app/Http/Controllers/Tecnico/TecnicoDocumentController.php
@@ -38,23 +38,23 @@ class TecnicoDocumentController extends Controller
     public function store(Request $request)
     {
 
-         // return response()->json(['data'=> $request->all(), 'status'=>'sucess'], 201);
 
        // $file = $request->file('file')->store('public/certificados');
 
 
 
       $file_data = $request->input('file');
+         return response()->json(['data'=> $file_data, 'status'=>'sucess'], 201);
       //generating unique file name;
-      $file_name = 'image_'.time().'.jpg';
+      // $file_name = 'image_'.time().'.jpg';
       //@list($type, $file_data) = explode(';', $file_data);
       //@list(, $file_data)      = explode(',', $file_data);
-      if($file_data!=""){
+      // if($file_data!=""){
         // storing image in storage/app/public Folder
 
-        $file = base64_decode($file_data);
-        \Storage::disk('public')->put($file_name, $file);     
-      }
+        // $file = base64_decode($file_data);
+        // \Storage::disk('public')->put($file_name, $file);     
+      // }
   
        // $create =  new TecnicoDocument;
        // $create->documento = 'nuevo documento';
