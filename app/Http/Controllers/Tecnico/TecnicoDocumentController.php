8664bcc61d4454c2a98c30721bcc3f91f507f17b
Jhoysner <jhoysner@hotmail.com>
2019-03-22 18:17:05 -0400

    fixed modulo certificados
    fixed modulo certificados

--- app/Http/Controllers/Tecnico/TecnicoDocumentController.php
+++ app/Http/Controllers/Tecnico/TecnicoDocumentController.php
@@ -40,18 +40,20 @@ class TecnicoDocumentController extends Controller
 
         $user = auth('api')->user();
         $file_data = $request->input('file');
-        $file_name = 'image_'.time().'.jpg';
-        
-        if($file_data!=""){
-        
-            $file = base64_decode($file_data);
+
+        foreach ($file_data as  $imagen) {
+
+            $file_name = 'image_'.time().rand(1,1000).'.jpg';
+          
+            $file = base64_decode($imagen);
             \Storage::disk('certificados')->put($file_name, $file);     
+
+            $create =  new TecnicoDocument;
+            $create->documento = $request->description;
+            $create->certificado = $file_name;
+            $create->user_id = $user->id;
+       
         }
-      
-        $create =  new TecnicoDocument;
-        $create->documento = $request->description;
-        $create->certificado = $file_name;
-        $create->user_id = $user->id;
        
         if($create->save()){
          return response()->json(['data'=> $create, 'status'=>'sucess'], 201);
